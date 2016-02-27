b961bbb94fe7cc9f17b1b4186b69be24a4fd0006
Игорь Щербаков <i.v.shcherbakov@gmail.com>
2016-02-27 19:30:06 +0600

    Update i18n/ru

--- i18n/ru.js
+++ i18n/ru.js
@@ -3,7 +3,7 @@
 	"touchero description": "Играй в Guitar Hero на сенсорном экране",
 	"vkasync description": "Твои аудиозаписи всегда с тобой",
 	"slogan": "from Siberia with ♥",
-	"Projects": "Projects",
+	"Projects": "Проекты",
 	"GitHub": "GitHub",
 	"vk": "vk",
 	"en": "en",
